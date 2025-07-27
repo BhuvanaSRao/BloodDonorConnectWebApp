f2bb95cb3e3df9bc27347a05e1a48bbf44ee3d39
BhuvanaSRao <bhuvanasrao2@gmail.com>
2025-07-27 20:12:22 +0530

    Create BloodDonorConnectApplication.java

---
+++ emergencyblooddonorconnect/src/main/java/com/example/blooddonorconnect/BloodDonorConnectApplication.java
@@ -0,0 +1,11 @@
+package com.example.blooddonorconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonorConnectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BloodDonorConnectApplication.class, args);
+    }
+}
